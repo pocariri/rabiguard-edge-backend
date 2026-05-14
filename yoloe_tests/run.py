1fc8ac3d64d538976de03eb08d00add5776ff9b4
heechan <benny06282329@gmail.com>
2026-05-14 17:28:23 +0900

    feat: YOLOE 테스트 코드 및 파일

--- yoloe_tests/run.py
+++ yoloe_tests/run.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLOE
 
 # Initialize a YOLOE model
-model = YOLOE("yoloe-26l-seg-pf.pt")
+model = YOLOE("yoloe-26n-seg-pf.pt")
 
 # Run prediction. No prompts required.
-results = model.predict("path/to/image.jpg")
+results = model.predict("./image.jpg")
 
 # Show results
 results[0].show()
